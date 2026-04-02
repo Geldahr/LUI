c8ea75ef486685136f9f6ff0579dbb946359fac0
Geldahr <geldahr@gmail.com>
2026-04-02 23:59:03 +0200

    Small status bar fix

--- src/StatusBar/widget_base.lua
+++ src/StatusBar/widget_base.lua
@@ -1,6 +1,7 @@
 import "LUI.src.UI.Widgets"
 
 local S = _G.STATUS_BAR_COMMON
+local ICON_MARGIN = 2
 
 local StatusBarWidgetBase = class(Turbine.UI.Control)
 _G.StatusBarWidgetBase = StatusBarWidgetBase
@@ -65,10 +66,10 @@ function StatusBarWidgetBase:Constructor(widget_key, widget_w, bar_h, font, cont
 
     if show_icon then
         local icon_y = S.get_centered_icon_y(h, icon_h)
-        self.icon:SetPosition(0, icon_y)
+        self.icon:SetPosition(ICON_MARGIN, icon_y)
         self.icon:set_icon(icon_path, icon_w, icon_h)
-        self.label:SetPosition(icon_w + S.ICON_GAP, 0)
-        self.label:SetSize(math.max(0, w - icon_w - S.ICON_GAP), h)
+        self.label:SetPosition(ICON_MARGIN + icon_w + S.ICON_GAP, 0)
+        self.label:SetSize(math.max(0, w - ICON_MARGIN - icon_w - S.ICON_GAP), h)
     else
         self.icon:SetVisible(false)
         self.label:SetPosition(0, 0)
