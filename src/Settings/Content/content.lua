12fe01fb36770bf30f6deb15b3f65edf11219e84
Geldahr <geldahr@gmail.com>
2026-05-02 18:41:30 +0200

    Add settings content base classes

---
+++ src/Settings/Content/content.lua
@@ -0,0 +1,120 @@
+import "LUI.src.Settings.Tabs.form_page"
+
+local SettingsFormPage = (_G.LUI_SETTINGS_SHARED ~= nil and _G.LUI_SETTINGS_SHARED.form_page) or
+    _G.SettingsFormPage or SettingsFormPage
+
+ConfigContent = class(SettingsFormPage)
+_G.ConfigContent = ConfigContent
+_G.LUI_SETTINGS_SHARED = _G.LUI_SETTINGS_SHARED or {}
+_G.LUI_SETTINGS_SHARED.config_content = ConfigContent
+
+local function _set_text_entry_methods(entry)
+    function entry:get_value()
+        return self.tb:GetText()
+    end
+
+    function entry:set_value(value)
+        self.tb:SetText(value)
+    end
+end
+
+local function _set_dropdown_entry_methods(entry)
+    function entry:get_value()
+        return self.button:GetValue()
+    end
+
+    function entry:set_value(value)
+        self.value = value
+        self.button:SetValue(value)
+    end
+end
+
+local function _set_checkbox_entry_methods(entry)
+    function entry:get_value()
+        return self.cb:IsChecked()
+    end
+
+    function entry:set_value(value)
+        self.cb:SetChecked(value)
+    end
+end
+
+function ConfigContent:Constructor(window, columns, refresh_preview_fn)
+    SettingsFormPage.Constructor(self, window)
+
+    self._bindings = {}
+    self:set_compact_fields(true)
+    if columns ~= nil then
+        self:set_grid_columns(columns)
+    end
+    if refresh_preview_fn ~= nil then
+        self.refresh_preview = refresh_preview_fn
+    end
+end
+
+function ConfigContent:bind(entry, save_fn, load_fn)
+    if type(save_fn) ~= "function" then
+        error("ConfigContent binding is missing save_fn")
+    end
+    if type(load_fn) ~= "function" then
+        error("ConfigContent binding is missing load_fn")
+    end
+
+    self._bindings[#self._bindings + 1] = {
+        entry = entry,
+        save = save_fn,
+        load = load_fn,
+    }
+
+    return entry
+end
+
+function ConfigContent:add_line_edit(label_text, key, save_fn, load_fn, help_text, full_width)
+    local entry = SettingsFormPage.add_text(self, key, label_text, false, help_text, full_width)
+    _set_text_entry_methods(entry)
+    return self:bind(entry, save_fn, load_fn)
+end
+
+function ConfigContent:add_color_picker(label_text, key, save_fn, load_fn, help_text, full_width)
+    local entry = SettingsFormPage.add_text(self, key, label_text, true, help_text, full_width)
+    _set_text_entry_methods(entry)
+    return self:bind(entry, save_fn, load_fn)
+end
+
+function ConfigContent:add_dropdown(label_text, key, option_labels, option_values, save_fn, load_fn, help_text,
+                                    full_width)
+    local entry = SettingsFormPage.add_dropdown(self, key, label_text, option_labels, option_values, help_text,
+        full_width)
+    _set_dropdown_entry_methods(entry)
+    return self:bind(entry, save_fn, load_fn)
+end
+
+function ConfigContent:add_checkbox(label_text, key, save_fn, load_fn, full_width)
+    local entry = SettingsFormPage.add_checkbox(self, key, label_text, full_width)
+    _set_checkbox_entry_methods(entry)
+    return self:bind(entry, save_fn, load_fn)
+end
+
+function ConfigContent:break_line()
+    return SettingsFormPage.add_break(self, 0)
+end
+
+function ConfigContent:load()
+    self.loading = true
+
+    for i = 1, #self._bindings do
+        local binding = self._bindings[i]
+        binding.load(binding.entry, self)
+    end
+
+    self.loading = false
+    self:update_all_swatches()
+    self:layout()
+end
+
+function ConfigContent:save()
+    for i = 1, #self._bindings do
+        local binding = self._bindings[i]
+        binding.save(binding.entry:get_value(), binding.entry, self)
+    end
+end
---
+++ src/Settings/Content/section_page.lua
@@ -0,0 +1,40 @@
+import "LUI.src.Settings.Tabs.feature_shell"
+
+local FeatureShell = (_G.LUI_SETTINGS_SHARED ~= nil and _G.LUI_SETTINGS_SHARED.feature_shell) or SettingsFeatureShell
+local SettingsFeatureSectionPage = FeatureShell.section_page_class
+
+ConfigSectionPage = class(SettingsFeatureSectionPage)
+_G.ConfigSectionPage = ConfigSectionPage
+_G.LUI_SETTINGS_SHARED = _G.LUI_SETTINGS_SHARED or {}
+_G.LUI_SETTINGS_SHARED.config_section_page = ConfigSectionPage
+
+function ConfigSectionPage:Constructor(window, preview_key, preview_height, preview_refresh_fn, use_button_tabs)
+    SettingsFeatureSectionPage.Constructor(self, window, preview_key, preview_height, preview_refresh_fn,
+        use_button_tabs)
+end
+
+function ConfigSectionPage:add_tab(text, key, page)
+    self:add_section(text, key, page)
+end
+
+function ConfigSectionPage:get_page(key)
+    return self._sections[key]
+end
+
+function ConfigSectionPage:load()
+    for i = 1, #self._section_order do
+        local page = self._sections[self._section_order[i]]
+        if page.load ~= nil then
+            page:load()
+        end
+    end
+end
+
+function ConfigSectionPage:save()
+    for i = 1, #self._section_order do
+        local page = self._sections[self._section_order[i]]
+        if page.save ~= nil then
+            page:save()
+        end
+    end
+end
