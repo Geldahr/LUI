f1228803e5ff13d2939daeac1f065a62ff90aa4b
Geldahr <geldahr@gmail.com>
2026-05-02 19:15:44 +0200

    Refactor legacy party settings wrapper

--- src/Settings/Tabs/Party/party_page.lua
+++ src/Settings/Tabs/Party/party_page.lua
@@ -1,24 +1,25 @@
-import "LUI.src.Settings.Tabs.tabbed_page"
+import "LUI.src.Settings.Content.tabs"
 import "LUI.src.Settings.Tabs.Party.party_layout"
 import "LUI.src.Settings.Tabs.Party.party_vitals"
 
-local SettingsTabbedPage = (_G.LUI_SETTINGS_SHARED ~= nil and _G.LUI_SETTINGS_SHARED.tabbed_page) or
-    _G.SettingsTabbedPage or SettingsTabbedPage
+local ConfigTabs = (_G.LUI_SETTINGS_SHARED ~= nil and _G.LUI_SETTINGS_SHARED.config_tabs) or ConfigTabs
 
-PartyPage = class(SettingsTabbedPage)
+PartyPage = class(ConfigTabs)
 
 function PartyPage:Constructor(window)
-    SettingsTabbedPage.Constructor(self, window)
+    ConfigTabs.Constructor(self, window)
     self.show_main_content_border = false
 
-    self:add_sub_page(TR["Layout"], PartyLayout)
-    self:add_sub_page(TR["Vitals"], PartyVitals)
+    self:add_tab(TR["Layout"], "layout", PartyLayout(window))
+    self:add_tab(TR["Vitals"], "vitals", PartyVitals(window))
 end
 
 function PartyPage:load_from_settings(s, ui)
-    self:load_pages(s, ui)
+    self:get_page("layout"):load_from_settings(s, ui)
+    self:get_page("vitals"):load_from_settings(s, ui)
 end
 
 function PartyPage:apply_to_settings(s, ui)
-    self:apply_pages(s, ui)
+    self:get_page("layout"):apply_to_settings(s, ui)
+    self:get_page("vitals"):apply_to_settings(s, ui)
 end
